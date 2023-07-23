9527530ae318d8183191311d5bf01ead9c3a6592
gregory <greg2dl101@yahoo.com>
2023-07-24 00:06:02 +0200

    Game ends after 5 rounds and declares winner

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //selects random item from array as computer selection
 function computerPlay() {
-  const selectionArray = ['ROCK', 'PAPER', 'SCISSORS'];
+  const selectionArray = ["ROCK", "PAPER", "SCISSORS"];
 
-  const random = selectionArray[Math.floor(Math.random() *
-      selectionArray.length)];
+  const random =
+    selectionArray[Math.floor(Math.random() * selectionArray.length)];
 
   return random;
 }
@@ -21,37 +21,37 @@ let computerScore = 0;
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toUpperCase();
 
-  if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
-    playerScore  = playerScore + 1;
-    return 'You Win! Rock beats Scissors!';
+  if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+    playerScore = playerScore + 1;
+    return "You Win! Rock beats Scissors!";
   }
-  if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
-    computerScore  = computerScore + 1;
-    return 'Computer Wins! Paper beats Rock!';
+  if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+    computerScore = computerScore + 1;
+    return "Computer Wins! Paper beats Rock!";
   }
-  if (playerSelection == 'ROCK' && computerSelection == 'ROCK') {
+  if (playerSelection == "ROCK" && computerSelection == "ROCK") {
     return "It's a tie!";
   }
-  if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
-    computerScore  = computerScore + 1;
-    return 'Computer Wins! Scissors beats Paper!';
+  if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
+    computerScore = computerScore + 1;
+    return "Computer Wins! Scissors beats Paper!";
   }
-  if (playerSelection == 'PAPER' && computerSelection == 'PAPER') {
+  if (playerSelection == "PAPER" && computerSelection == "PAPER") {
     return "It's a tie!";
   }
-  if (playerSelection == 'PAPER' && computerSelection == 'ROCK') {
-    playerScore  = playerScore + 1;
+  if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+    playerScore = playerScore + 1;
     return "You Win! Paper beats Rock!";
   }
-  if (playerSelection == 'SCISSORS' && computerSelection == 'SCISSORS') {
+  if (playerSelection == "SCISSORS" && computerSelection == "SCISSORS") {
     return "It's a tie!";
   }
-  if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
-    playerScore  = playerScore + 1;
+  if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+    playerScore = playerScore + 1;
     return "You Win! Scissors beats Paper!";
   }
-  if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
-    computerScore  = computerScore + 1;
+  if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
+    computerScore = computerScore + 1;
     return "Computer Wins! Rock beats Scissors!";
   }
 }
@@ -65,15 +65,26 @@ function game() {
   console.log(playerScore, computerScore);
 }
 
-//plays game to 5 using game function
-while (playerScore < 5 && computerScore < 5) {
-  game();
+//function that declares a winner
+function declareWinner() {
+  let gameCount = 0;
+
+  //plays game to 5 using game function
+  while (gameCount < 5) {
+    for (let i = 1; i <= 5; i++) {
+      game();
+      gameCount = i;
+      console.log(gameCount);
+
+      if (gameCount == 5) {
+        if (playerScore > computerScore) {
+          return "You Win!";
+        } else {
+          return "Computer Wins!";
+        }
+      }
+    }
+  }
 }
 
-//Displays winner in alert message in browser
-if (playerScore >= 5) {
-  alert("You Win!");
-}
-else {
-  alert("Computer Wins!");
-}
+alert(declareWinner());
