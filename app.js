8c3594f5b0bfa3b2def0642451b131b844c9c60e
gregory <greg2dl101@yahoo.com>
2023-07-29 11:16:37 +0200

    Added color and reset to scores

--- app.js
+++ app.js
@@ -2,23 +2,19 @@ const buttons = document.querySelectorAll("button");
 const resultDisplay = document.querySelector("#result-display");
 const userDisplay = document.querySelector("#user-display");
 const computerDisplay = document.querySelector("#computer-display");
-const displayWinner = document.querySelector("#display-winner");
 
 let playerSelection;
 let computerSelection;
 
-//score variables
 let playerScore = 0;
 let computerScore = 0;
 
-//selects random item from array as computer selection
 function computerPlay() {
   const selectionArray = ["ROCK", "PAPER", "SCISSORS"];
 
   return selectionArray[Math.floor(Math.random() * selectionArray.length)];
 }
 
-//determines winner based on player inputs and cp selection
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toUpperCase();
 
@@ -57,7 +53,6 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-//uses two callbacks to play a round of rock, paper, scissors
 buttons.forEach((button) =>
   button.addEventListener("click", (e) => {
     computerSelection = computerPlay();
@@ -69,12 +64,14 @@ buttons.forEach((button) =>
 
     computerDisplay.innerHTML = computerScore;
 
-    displayWinner.innerHTML = declareWinner(playerScore, computerScore);
+    if (playerScore >= 5 || computerScore >= 5) {
+      resultDisplay.innerHTML = declareWinner(playerScore, computerScore);
+      resetScores();
+    }
   })
 );
 
-// Declares winner after 5 rounds!!!
-function declareWinner(playerScore, computerScore) {
+function declareWinner() {
   if (playerScore >= 5 || computerScore >= 5) {
     if (playerScore > computerScore) {
       return "You Win!";
@@ -83,6 +80,11 @@ function declareWinner(playerScore, computerScore) {
       return "You Lose!";
     }
   } else {
-    return "Keep Playing!";
+    return "";
   }
+}
+
+function resetScores() {
+  playerScore = 0;
+  computerScore = 0;
 }
